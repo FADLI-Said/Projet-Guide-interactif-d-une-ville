6aef3cc84a86979e4deaffa0f909e42d15f17cb9
FADLI-Said <saidfadli224@gmail.com>
2024-12-17 11:26:26 +0100

    Sans num

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 i = 0;
 
 fetch("data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        data.restaurants.forEach((item) => {
-            console.log(item);
-            document.getElementById("resto").addEventListener("click", function () {
-                document.querySelector("#content").innerHTML += `
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.restaurants.forEach((item) => {
+      console.log(item);
+      document.getElementById("resto").addEventListener("click", function () {
+        document.querySelector("#content").innerHTML += `
                 <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
                     <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
                     <div class="card-body">
@@ -24,8 +24,8 @@ fetch("data.json")
       </div>
       <div class="modal-body">
         <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
-        <p class="card-text">${item.description}</p>
-        <p class="card-text">${item.adresse}</p>
+        <p class="card-text text-center">${item.description}</p>
+        <p class="card-text text-center">${item.adresse}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -34,22 +34,22 @@ fetch("data.json")
   </div>
 </div>
             `;
-                i++
-            });
-        });
-    })
-    .catch((error) => console.error(error));
+        i++
+      });
+    });
+  })
+  .catch((error) => console.error(error));
 
 fetch("data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        data.adresses.forEach((item) => {
-            console.log(item);
-            document
-                .getElementById("adresses")
-                .addEventListener("click", function () {
-                    document.querySelector("#content").innerHTML += `
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.adresses.forEach((item) => {
+      console.log(item);
+      document
+        .getElementById("adresses")
+        .addEventListener("click", function () {
+          document.querySelector("#content").innerHTML += `
                 <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
                     <img src="${item.image}" class="img-fluid" alt="une image de ${item.name}">
                     <div class="card-body">
@@ -66,8 +66,8 @@ fetch("data.json")
       </div>
       <div class="modal-body">
         <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
-        <p class="card-text">${item.description}</p>
-        <p class="card-text">${item.adresse}</p>
+        <p class="card-text text-center">${item.description}</p>
+        <p class="card-text text-center">${item.adresse}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -76,22 +76,22 @@ fetch("data.json")
   </div>
 </div>
             `;
-                i++
-                });
+          i++
         });
-    })
-    .catch((error) => console.error(error));
+    });
+  })
+  .catch((error) => console.error(error));
 
 fetch("data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        data.culture.forEach((item) => {
-            console.log(item);
-            document
-                .getElementById("culturel")
-                .addEventListener("click", function () {
-                    document.querySelector("#content").innerHTML += `
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.culture.forEach((item) => {
+      console.log(item);
+      document
+        .getElementById("culturel")
+        .addEventListener("click", function () {
+          document.querySelector("#content").innerHTML += `
                 <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
                     <img src="${item.image}" class="img-fluid" alt="une image de ${item.name}">
                     <div class="card-body">
@@ -108,8 +108,8 @@ fetch("data.json")
       </div>
       <div class="modal-body">
         <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
-        <p class="card-text">${item.description}</p>
-        <p class="card-text">${item.adresse}</p>
+        <p class="card-text text-center">${item.description}</p>
+        <p class="card-text text-center">${item.adresse}</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -118,24 +118,133 @@ fetch("data.json")
   </div>
 </div>
             `;
-            i++
-                });
+          i++
         });
-    })
-    .catch((error) => console.error(error));
+    });
+  })
+  .catch((error) => console.error(error));
 
-document.getElementById("culturel").addEventListener("click", function () {
-    document.querySelector("#content").innerHTML = "";
-});
 
 document.getElementById("resto").addEventListener("click", function () {
-    document.querySelector("#content").innerHTML = "";
+  document.querySelector("#content").innerHTML = "";
+  document.getElementById("minTitle").innerText = "Restaurants"
+});
+
+document.getElementById("culturel").addEventListener("click", function () {
+  document.querySelector("#content").innerHTML = "";
+  document.getElementById("minTitle").innerText = "Sortie Culturel"
 });
 
 document.getElementById("adresses").addEventListener("click", function () {
-    document.querySelector("#content").innerHTML = "";
+  document.querySelector("#content").innerHTML = "";
+  document.getElementById("minTitle").innerText = "Bonnes adresses"
 });
 
 
 
-/* <p class="card-text">${item.description}</p> */
+
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.restaurants.forEach((item) => {
+      console.log(item);
+
+        document.querySelector("#content").innerHTML += `
+                <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+                    <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                    <div class="card-body">
+                        <p class="fs-5 card-title">${item.name}</p>
+                    </div>
+                </div>
+
+                <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">${item.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                        <p class="card-text text-center">${item.description}</p>
+                        <p class="card-text text-center">${item.adresse}</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+        i++
+      ;
+    })
+    
+    data.culture.forEach((item) => {
+      console.log(item);
+
+        document.querySelector("#content").innerHTML += `
+                <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+                    <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                    <div class="card-body">
+                        <p class="fs-5 card-title">${item.name}</p>
+                    </div>
+                </div>
+
+                <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">${item.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                        <p class="card-text text-center">${item.description}</p>
+                        <p class="card-text text-center">${item.adresse}</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+        i++
+      ;
+    })
+    
+    data.adresses.forEach((item) => {
+      console.log(item);
+
+        document.querySelector("#content").innerHTML += `
+                <div class="card col-12 col-lg-3 m-3 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+                    <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                    <div class="card-body">
+                        <p class="fs-5 card-title">${item.name}</p>
+                    </div>
+                </div>
+
+                <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">${item.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <img src="${item.image}" class="img-fluid" alt="une image d'une ${item.name}">
+                        <p class="card-text text-center">${item.description}</p>
+                        <p class="card-text text-center">${item.adresse}</p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+        i++
+      ;
+    });
+  })
+  .catch((error) => console.error(error));
